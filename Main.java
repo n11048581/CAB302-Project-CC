47e53d85fea87688b4b872a73cb14b8c84eadfe4
Eden-Channon <115522197+Eden-Channon@users.noreply.github.com>
2024-09-11 23:15:08 +1000

    Reworked login, not perfect
    Going to change it all again anyway

---
+++ Main.java
@@ -0,0 +1,24 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Log-In.fxml"));
+        stage.setTitle("Log In");
+        stage.setScene(new Scene(root, 600,400));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
