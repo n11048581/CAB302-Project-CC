02f3b450250243994b951783e388d6fd9a257e23
Eden Channon <edenchannon@gmail.com>
2024-09-14 18:09:36 +1000

    Cleanerer Code (Method to change scene added)

--- src/main/java/test/fuelapp/LoginController.java
+++ src/main/java/test/fuelapp/LoginController.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
