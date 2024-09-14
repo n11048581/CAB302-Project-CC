f112da66831c42e2fca9452d0c69fcccfd7ac2fc
Eden Channon <edenchannon@gmail.com>
2024-09-14 18:53:57 +1000

    Cleaner-er Code (Method to change scene added)

--- src/main/java/test/fuelapp/SignUpController.java
+++ src/main/java/test/fuelapp/SignUpController.java
@@ -1,22 +1,12 @@
 package test.fuelapp;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class SignUpController implements Initializable {
-    public LoginModel loginModel = new LoginModel();
     public SQLiteLink sqLiteLink = new SQLiteLink();
-
-    @FXML
-    private TextField tf_username;
-
-    @FXML
-    private TextField tf_password;
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
