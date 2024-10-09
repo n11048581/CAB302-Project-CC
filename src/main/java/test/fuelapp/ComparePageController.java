9a7d2f1283884c5a5fdc85b4a64c03e1c80652fe
Eden Channon <edenchannon@gmail.com>
2024-10-09 18:43:33 +1000

    Price compare page now updates from the database rather than reading from API, making much faster. Other functionality still needs to be repaired to work with this system and code reformatted

--- src/main/java/test/fuelapp/ComparePageController.java
+++ src/main/java/test/fuelapp/ComparePageController.java
@@ -8,10 +8,23 @@ import javafx.scene.layout.VBox;
 import test.fuelapp.sample.FuelPriceAPI;
 import test.fuelapp.sample.FuelPriceAPI.StationDetails;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class ComparePageController {
+    Connection connection;
+
+    public ComparePageController() {
+        connection = SQLiteLink.Connector();
+        if (connection == null) {
+            System.out.println("Database Connection Unsuccessful");
+            System.exit(1);
+        }
+    }
 
     @FXML
     private TextField searchBar;
@@ -32,9 +45,9 @@ public class ComparePageController {
     private FuelPriceAPI fuelPriceAPI = new FuelPriceAPI(); // Create an instance of FuelPriceAPI
 
     @FXML
-    private void initialize() {
+    private void initialize() throws SQLException {
         // Initialize the station data from FuelPriceAPI
-        fuelPriceAPI.getStationsData();  // Populate the data
+        //fuelPriceAPI.getStationsData();  // Populate the data
         handlePriceCompare();
     }
 
@@ -47,6 +60,9 @@ public class ComparePageController {
         List<StationDetails> filteredStations = fuelPriceAPI.getStationsMap().values().stream()
                 .filter(station -> station.getName().contains(searchQuery) || station.getAddress().contains(searchQuery))
                 .collect(Collectors.toList());
+
+        System.out.println(filteredStations);
+
 
         // Clear the comparePriceBox
         comparePriceBox.getChildren().clear();
@@ -63,28 +79,60 @@ public class ComparePageController {
     }
 
     @FXML
-    public void handlePriceCompare() {
+    public void handlePriceCompare() throws SQLException {
         // Get the list of StationDetails objects
-        List<StationDetails> stations = fuelPriceAPI.getStationsMap().values().stream().collect(Collectors.toList());
-
-        // Clear the comparePriceBox
+        //List<StationDetails> stations = fuelPriceAPI.getStationsMap().values().stream().collect(Collectors.toList());
         comparePriceBox.getChildren().clear();
 
-        // Iterate over the StationDetails objects and add a label for each one
-        for (StationDetails station : stations) {
-            Label label = new Label("                       "
-                    +"Station: " + station.getName() +
-                    " - Price: " + station.getPrice() +
-                    " - Address: " + station.getAddress() +
-                    " - Fuel type: " + station.getFuelType()
-                    +" - Distance: " + station.getDistance()
-                    +" - Travel cost: " + station.getTravelCost());
-            comparePriceBox.getChildren().add(label);
-            comparePriceBox.getChildren().add(new Separator());
+        PreparedStatement prepStatement = null;
+        ResultSet resultSet = null;
+        // Set SQL command to match user input against database
+        String query = "SELECT * FROM gas_stations";
+        try {
+            // Execute query on entered username and password
+            prepStatement = connection.prepareStatement(query);
+            resultSet = prepStatement.executeQuery();
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("station_name"));
+
+                Label label = new Label("                       "
+                +"Station: " + resultSet.getString("station_name") +
+                " - Price: " + resultSet.getDouble("price") +
+                " - Address: " + resultSet.getString("station_address") +
+                " - Fuel type: " + resultSet.getString("fuel_type"));
+                //+" - Distance: " + station.getDistance()
+                //  +" - Travel cost: " + station.getTravelCost());
+                comparePriceBox.getChildren().add(label);
+                comparePriceBox.getChildren().add(new Separator());
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Close prepared statement
+            //prepStatement.close();
+            //resultSet.close();
         }
+        
+
+        // Clear the comparePriceBox
+
+        // Iterate over the StationDetails objects and add a label for each one
+      //  for (StationDetails station : stations) {
+       //     Label label = new Label("                       "
+       //             +"Station: " + station.getName() +
+       //             " - Price: " + station.getPrice() +
+      //              " - Address: " + station.getAddress() +
+       //             " - Fuel type: " + station.getFuelType()
+      //              +" - Distance: " + station.getDistance()
+      //              +" - Travel cost: " + station.getTravelCost());
+     //       comparePriceBox.getChildren().add(label);
+       //     comparePriceBox.getChildren().add(new Separator());
+        //}
     }
 
-    @FXML
+
     public void map(ActionEvent event) {
         // Go back to the landing page
         sqLiteLink.changeScene(event, "LandingPage.fxml", "Landing Page");
