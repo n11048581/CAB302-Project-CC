213a14bc53e9fe131aa8bee16418c9b138fcac3e
chmmcc <162306178+chmmcc@users.noreply.github.com>
2024-10-11 15:16:51 +1000

    Add Javadoc generation and publishing steps

---
+++ docs/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"test.fuelapp","l":"test.fuelapp"},{"m":"test.fuelapp","l":"test.fuelapp.sample"}];updateSearchResults();
