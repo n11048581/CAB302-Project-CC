7f142da07025f4e790af26fc1a955dcbb14aac85
laual <n11048581@qut.edu.au>
2024-10-24 13:12:02 +1000

    added database connection test

--- src/test/java/DatabaseTest.java
+++ src/test/java/DatabaseTest.java
@@ -1,14 +1,16 @@
 import org.junit.jupiter.api.*;
+import test.fuelapp.SQLiteLink;
 
 import java.sql.Connection;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DatabaseTest {
-    /*
+
     @Test
     public void testConnection() {
-        Connection conn = SqliteConnection.getInstance();
-        assertEquals(true, conn != null);
+        Connection conn = SQLiteLink.Connector();
+        assertNotNull(conn);
     }
-     */
+
 }
